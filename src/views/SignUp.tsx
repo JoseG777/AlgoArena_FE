09ae6982b055ae0399c683585f6c2fe8ce109ead
JoseG777 <josegc7773@gmail.com>
2025-09-24 11:51:56 -0400

    Make identifying fields in sign up lowercase

--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -36,8 +36,8 @@ const SignUp: React.FC = ()=>
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                username: formData.username,
-                email: formData.email,
+                username: formData.username.toLowerCase(),
+                email: formData.email.toLowerCase(),
                 password: formData.password,
             }),
             });
