7cec65cf7bb34a0c31f6f11b32eed15de9c7f2e7
JoseG777 <josegc7773@gmail.com>
2025-09-25 19:33:38 -0400

    Add auth re-route logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,43 @@ import HomePage from './views/HomePage';
 import NotHomePage from './views/NotHomePage';
 import SignUp from './views/SignUp';
 import SignIn from './views/SignIn';
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicOnlyRoute from './components/PublicOnlyRoute';
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/test" element={<NotHomePage/>}/>
-        <Route path="sign-up" element={<SignUp/>}/>
-        <Route path="sign-in" element={<SignIn/>}/>
+        {/* Public Routes */}
+        <Route path="/" element={
+          <PublicOnlyRoute to="/test">
+            <HomePage/>
+          </PublicOnlyRoute>
+          }/>
+
+        <Route path="sign-up" element={
+          <PublicOnlyRoute to="/test">
+            <SignUp/>
+          </PublicOnlyRoute>
+          }/>
+
+        <Route path="sign-in" element={
+          <PublicOnlyRoute to="/test">
+            <SignIn/>
+          </PublicOnlyRoute>
+          }/>
+
+        {/* Protected Routes */}
+        <Route
+          path="/test"
+          element={
+            <ProtectedRoute>
+              <NotHomePage />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   )
--- src/views/NotHomePage.tsx
+++ src/views/NotHomePage.tsx
@@ -1,8 +1,17 @@
+import { useAuth } from "../context/AuthContext";
+
 const NotHomePage: React.FC = () =>
 {
+    const {user} = useAuth();
     return(
         <div>
             <h1> This file was created for testing purposes. </h1>
+            <br/>
+            <h2>{user?.sub}</h2>
+            <br/>
+            <h2>{user?.username}</h2>
+            <br/>
+            <h2>{user?.email}</h2>
         </div>
     )
 }
